9e1fdd69da0f18e8bac44d56e711892c11d88bef
Jesse <jesselcorbett@gmail.com>
2015-02-21 15:13:08 -0600

    Initial Commit

---
+++ android/src/doggame/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package doggame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import doggame.DogGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DogGame(), config);
+	}
+}
---
+++ desktop/src/doggame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package doggame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import doggame.DogGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DogGame(), config);
+	}
+}
