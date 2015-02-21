55f0c7310494624f72ce7b08cc9d63823ceadb9a
Jesse <jesselcorbett@gmail.com>
2015-02-21 16:58:13 -0600

    Minor changes

---
+++ desktop/src/dgame/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package dgame.desktop;
+
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dgame.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
