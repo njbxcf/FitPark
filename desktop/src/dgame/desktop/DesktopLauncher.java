864b761ebfbe3ebe7ecf4fa43c5a29d107fe1d49
Jesse <jesselcorbett@gmail.com>
2015-02-22 10:16:00 -0600

    Added images and change to desktop resolution to match app expectations.

--- desktop/src/dgame/desktop/DesktopLauncher.java
+++ desktop/src/dgame/desktop/DesktopLauncher.java
@@ -8,6 +8,8 @@ import dgame.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1280;
+        config.height = 720;
 		new LwjglApplication(new Game(), config);
 	}
 }
